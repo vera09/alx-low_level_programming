ba3821b55d3c86146c5d2bf1ff384da137b43b40
vera09 <vafrahsarfo@gmail.com>
2023-04-21 18:39:44 +0000

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: zero if no errors, non-zero if errors
+ */
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
