55d2f66ad5d9759dc0c205028bec5140bd1784ce
vera09 <vafrahsarfo@gmail.com>
2023-04-21 18:18:05 +0000

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry for program
+ *
+ * Return: zero if no errors, Non zero if others
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
