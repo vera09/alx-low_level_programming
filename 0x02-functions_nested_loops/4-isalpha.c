8412fd4d4f8fa1d26a168b7c7dfb3d1bb6901128
vera09 <vafrahsarfo@gmail.com>
2023-04-26 18:05:09 +0000

    closed the comments

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,6 +3,7 @@
  * _isalpha - check if a variable is an alphabet 
  * @c: the variable to be checked
  * Return: 1 if correct, zero if error
+ */
 int _isalpha(int c)
 {
 	if (c >= 'A'  && c <= 'Z') || (c >='a' && c <='z')
