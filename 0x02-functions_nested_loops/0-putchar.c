04e3990e1cfc080441b99f3dd1a82d2074629e32
vera09 <vafrahsarfo@gmail.com>
2023-04-26 17:14:44 +0000

    Task ONe functions and nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <main.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: Zero if sucess, no zero if errors'
+ */
+
+int main(void)
+{
+	_putchar(95);
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('/n');
+
+	return (0);
+}
