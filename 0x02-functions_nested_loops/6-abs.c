6fbc733430d078b96ac371584f3c2c25e8c1454c
vera09 <vafrahsarfo@gmail.com>
2023-04-26 18:22:11 +0000

    Task 5 and 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - prints the sign of a number
+ * @n: number to print
+ * Return: the absolute value of the n
+ */
+int _abs(int n)
+{
+	if(n >= 0)
+	{
+	return(n);
+	}
+	else
+	{
+	return (-n);
+	}
+}
